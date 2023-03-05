8b6cad9ee3045e9c6dec5b72aa401d7be31682a1
Brandon Lamb <blamb888@gmail.com>
2023-03-06 00:28:43 +0900

    add basic init func

---
+++ code/main.py
@@ -0,0 +1,19 @@
+import pygame, sys
+
+# pygame setup
+pygame.init()
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.fill('black')
+    
+    pygame.display.update()
+    clock.tick(60)
