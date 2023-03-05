4aec6e072624a956bf5add3d4794c11a67b03cae
Brandon Lamb <blamb888@gmail.com>
2023-03-06 00:42:29 +0900

    make level func and import game_data for level 0

---
+++ code/game_data.py
@@ -0,0 +1,9 @@
+level_0 = {
+		'terrain': '../levels/0/level_0_terrain.csv',
+		'coins':'../levels/0/level_0_coins.csv',
+		'fg trees':'../levels/0/level_0_fg_trees.csv',
+		'bg trees':'../levels/0/level_0_bg_trees.csv',
+		'enemies':'../levels/0/level_0_enemies.csv',
+		'constraints':'../levels/0/level_0_constraints.csv',
+		'player': '../levels/0/level_0_player.csv',
+		'decorations': '../levels/0/level_0_decorations.csv'}
--- code/main.py
+++ code/main.py
@@ -1,11 +1,13 @@
 import pygame, sys
+from settings import *
+from level import Level
+from game_data import level_0
 
 # pygame setup
 pygame.init()
-screen_width = 1280
-screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_0, screen)
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +16,7 @@ while True:
             sys.exit()
     
     screen.fill('black')
+    level.run()
     
     pygame.display.update()
     clock.tick(60)
